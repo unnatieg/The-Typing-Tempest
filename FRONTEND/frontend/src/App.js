c6dea6e5132f1e7797f4cf2c679bedd0c2f59886
Unnati Gupta <unnatigupta.uni@gmail.com>
2024-07-19 21:50:29 +0530

    updated app.js

--- FRONTEND/frontend/src/App.js
+++ FRONTEND/frontend/src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
-import IntensePage from './components/IntensePage';
-import IntenseWriting from './components/IntenseWriting';
-import AfterFailing from './components/AfterFailing';
-import { Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-
-<Routes>
-  <Route exact path='/' element={<HomePage/>}/>
-  <Route exact path='/intense' element={<IntensePage/>}/>
-  <Route exact path='/intenseWriting' element={<IntenseWriting/>}/>
-  <Route exact path='/afterfailing' element={<AfterFailing/>}/>
-
-</Routes>
-
+    <div className="App">
+      <HomePage />
+    </div>
   );
 }
 
